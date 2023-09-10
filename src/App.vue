5eae35df3d0a10adeacb556681b1f21c3d8f39a2
Namrata Sivakumar <43491187+namratasiv@users.noreply.github.com>
2023-09-10 21:10:33 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -90,7 +90,7 @@
     },
   },
   created(){
-const socket = io('http://127.0.0.1:8080/');
+const socket = io('https://1f1d-2406-7400-bd-447e-b825-383d-36c3-bdb7.ngrok-free.app/');
 console.log("new data came in!!!")
 
 socket.on('connect', function () {
