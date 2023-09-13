bd2bf887d3273177349a39ef317e8e00a7f9bcfa
Namrata Sivakumar <43491187+namratasiv@users.noreply.github.com>
2023-09-13 11:15:04 +0530

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,69 @@
 
 <template>
-  <h1> We'll be back 🤓 </h1>
+  <body style = "margin-left: 10px !important;">
+    <div class="container">
+    <section class="graph-two">
+
+    
+    <h1 style="position:relative; left: 380px; top: 30px;">ROADSHOW FEEDBACK 🎬</h1>
+  <div class="bar-graph-container">
+    
+    <div class="bar-container">
+      
+     <div class="html" v-bind:style="{ width: update3*0.1 + '%' }">
+      
+      </div>
+      
+    </div>
+    <div class ="stat-text" style="font-size: 3em;">😁 {{ update3 }}</div>
+   <div class="bar-container">
+     <div class="css" v-bind:style="{ width: update2*0.1 + '%' }">
+      
+      </div>
+    </div>
+    <div class ="stat-text" style="font-size: 3em;">😶 {{ update2 }}</div>
+  	<div class="bar-container">
+     <div class="js" v-bind:style="{ width: update1*0.1 + '%' }">
+      
+      </div>
+    </div>
+    <div class ="stat-text" style="font-size: 3em;">🙁 {{ update1 }}</div>
+
+   </div>
+  </section>
+</div>
+
+</body>
+<footer>
+
+  <div v-if="dataa"  id = "page1">
+    <video id = "vid1" class="fadeinout" width="300" height="300" autoplay >
+  <source src="./assets/SAD.mov" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+</div>
+    
+
+  <div v-if="data2" id = "page2">
+    <!-- <h1>Hello 2!</h1> -->
+    <!-- <h1>{{ status }}</h1> -->
+    <video id = "vid2" class="fadeinout" width="500" height="500" autoplay >
+  <source src="./assets/neutraL.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+    
+  </div>
+  <div v-if="data3"  id = "page3">
+    <!-- <h1>Hello 3!</h1> -->
+    <!-- <h1>{{ status }}</h1> -->
+    <video id = "vid3" class="fadeinout" width="500" height="500" autoplay >
+  <source src="./assets/smile.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+    
+  </div>
+
+</footer>
 </template>
 
 
@@ -46,10 +109,11 @@ socket.on('connect', function () {
       if (data.message == "1"){
         console.log("inside data message 1")
         this.dataa = true;
+        //document.getElementById("vid1").play();
         console.log(this.dataa)
       setTimeout(() => {
         this.dataa = false;
-      }, 5000);
+      }, 2000);
       } else{
         console.log("Else Block Socket Message is:", data.message)
       }
@@ -60,14 +124,14 @@ socket.on('connect', function () {
         this.data2=true;
         setTimeout(() => {
         this.data2 = false;
-      }, 5000);
+      }, 2000);
     });
     socket.on('insert3', (data) => {
       console.log("inside insert 2")
         this.data3=true;
         setTimeout(() => {
         this.data3 = false;
-      }, 5000);
+      }, 2000);
     });
 
     socket.on('update', (data) => {
